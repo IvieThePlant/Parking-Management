76f6ca2d1121cf979dac11b8dae8bec9d7f0a7e3
Lugman Abdi <abdill8@augsburg.edu>
2026-03-01 18:04:38 -0600

    Add basic authentication setup

--- parkingmanagement/urls.py
+++ parkingmanagement/urls.py
@@ -16,7 +16,16 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.contrib.auth import views as auth_views
+from django.shortcuts import redirect
+
+def home_redirect(request):
+    return redirect('login')
 
 urlpatterns = [
+    path('', home_redirect),  # visiting "/" sends you to login
     path('admin/', admin.site.urls),
+
+    path('login/', auth_views.LoginView.as_view(template_name='accounts/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 ]
