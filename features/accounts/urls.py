6fc29dcd07c42044690233b0ff1e809cf4246d60
Lugman Abdi <abdill8@augsburg.edu>
2026-04-07 07:41:59 -0500

    Add dashboard template

--- features/accounts/urls.py
+++ features/accounts/urls.py
@@ -26,4 +26,5 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='accounts/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(next_page='/admin'), name='logout'),
     path("register/", views.register_view, name="register"),
+    path('dashboard/', views.dashboard_view, name='dashboard'),
 ]
--- features/accounts/views.py
+++ features/accounts/views.py
@@ -6,6 +6,8 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 from .forms import RegisterForm
+from django.contrib.auth.decorators import login_required
+from parking.models import ParkingLot
 
 #Override register function to redirect user to specific pages.
 def register_view(request):
@@ -19,3 +21,10 @@ def register_view(request):
     else:
         form = RegisterForm()
     return render(request, "accounts/register.html", {"form": form})
+
+
+#Dashboard available only to logged in users
+@login_required
+def dashboard_view(request):
+    lots = ParkingLot.objects.all()
+    return render(request, 'accounts/dashboard.html', {'lots': lots})
