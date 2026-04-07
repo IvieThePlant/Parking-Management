b28fe271f99bdb956358dc67075406b899e7517b
Nur Atto <atton@augsburg.edu>
2026-04-07 14:30:43 -0500

    Fix createSession method to use lot_name instead of lot_id and return useful information instead of session object

--- parking/views.py
+++ parking/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from django.shortcuts import render
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated
@@ -10,35 +11,48 @@ from .service import ParkingSessionToJSON
 
 # Create Session using POST
 # URL - {domainName}/api/sessions/create/
-# BODY - {"spot_id" : ?, "end_time": ?}
-# RESPONSE (JSON) - {(session object)}
+# BODY - {"spot_number" : ?, "lot_name": ?}
+# RESPONSE (JSON) - {{result}}
 
 @api_view(['POST'])
 @permission_classes([IsAuthenticated])
 def createSession(request):
-    user = request.user
-    spot_id = request.data.get('spot_id')
-    end_time = request.data.get('end_time')
+    user=request.user
+    spot_number = request.data.get('spot_number')
+    lot = request.data.get('lot_name')
 
-    #Check to see if request includes spot_id in body (Tested Using PostMan)
-    if not spot_id:
-        return Response({"ERROR": "spot_id & end_time is needed in body"}, status=400)
+    #Check to see if request includes spot_number, end_time and lot_name in body (Tested Using PostMan)
+    if not spot_number or not lot:
+        return Response({"ERROR": "spot_number & lot_name is needed in body"}, status=400)
 
     try:
-        spot = ParkingSpot.objects.get(id=spot_id)
+        #GET BY SPOT_NUMBER AND LOT_NAME INSTEAD OF ID( __ )
+        spot = ParkingSpot.objects.get(spot_number=spot_number, lot__name=lot)
 
         # Check to see if spot is available
-        if not spot.is_available:
+        if not spot.available:
             return Response({"ERROR": "Spot is not available"}, status=400)
 
         # Create Session
+        #(TESTING USER) temp_user = User.objects.get(username="Temp")
         session = ParkingSession.objects.create(user=user, spot=spot)
 
         # Update spot to occupied so to prevent double booking
-        spot.is_available = False
+        spot.available = False
         spot.save()
+
+        result = []
+        result.append({
+            'user_id': session.user.id,
+            'session_id': session.id,
+            'spot_number': session.spot.spot_number,
+            'lot_name': session.spot.lot.name,
+            'start_time': session.start_time,
+            'end_time': session.end_time,
+            'status': session.status
+        })
         # Return sessions as JSON
-        return Response(ParkingSessionToJSON(session).data, status=201)
+        return Response(result, status=201)
 
     except ParkingSpot.DoesNotExist:
         # Return sessions as JSON
