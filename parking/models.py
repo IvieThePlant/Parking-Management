83c1391d0af46d840c5fa71c0a8dadeaf4ed440e
Nur Atto <atton@augsburg.edu>
2026-04-06 22:03:48 -0500

    (Temporary) Create ParkingSession table to work on api/crud Methods

--- parking/models.py
+++ parking/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class ParkingLot(models.Model):
     name = models.CharField(max_length=100, unique=True)
@@ -17,3 +18,13 @@ class ParkingSpot(models.Model):
 
     def __str__(self):
         return f"{self.lot.name} - Spot {self.spot_number}"
+
+class ParkingSession(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    spot = models.ForeignKey(ParkingSpot, on_delete=models.CASCADE)
+    start_time = models.DateTimeField(auto_now_add=True)
+    end_time = models.DateTimeField(null=True, blank=True)
+    status = models.CharField(max_length=10, default='active')
+
+    def __str__(self):
+        return f" User {self.user} - Spot {self.spot} - Status {self.status}"
