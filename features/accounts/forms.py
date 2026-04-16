097697a41265a21f74508b08609099a16d2ca1e7
Lugman Abdi <abdill8@augsburg.edu>
2026-04-16 11:01:14 -0500

    Removing dashboard from account path

--- features/accounts/forms.py
+++ features/accounts/forms.py
@@ -4,10 +4,25 @@ from django.contrib.auth.forms import UserCreationForm
 
 #Override the default registration form to include email input.
 class RegisterForm(UserCreationForm):
-    name = forms.CharField(label="Name", max_length=100)
-    username = forms.CharField(label="Username", max_length=100)
-    email = forms.EmailField(required=True)
+    name = forms.CharField(widget=forms.TextInput(attrs={
+        'class': 'block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 outline-gray-300'
+    }))
+    email = forms.EmailField(widget=forms.EmailInput(attrs={
+        'class': 'block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 outline-gray-300'
+    }))
 
     class Meta:
         model = User
-        fields = ["name", "username", "email", 'password1', 'password2']
+        fields = ['name', 'username', 'email', 'password1', 'password2']
+        widgets = {
+            'username': forms.TextInput(attrs={
+                'class': 'block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 outline-gray-300'
+            }),
+        }
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field in ['password1', 'password2']:
+            self.fields[field].widget.attrs.update({
+                'class': 'block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 outline-gray-300'
+            })
