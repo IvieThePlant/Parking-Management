e8ace2742227632c55952d676bd2cb033a3cc97e
brownbb9 <brownbb9@augsburg.edu>
2026-04-16 10:21:36 -0500

    Data update

--- testmapjs/map.js
+++ testmapjs/map.js
@@ -13,6 +13,18 @@ const map = new mapboxgl.Map({
 // https://docs.mapbox.com/mapbox-gl-js/guides/add-your-data/markers/
 const locations = [
   {
+    coordinates: [-93.242878, 44.964812],
+    name: 'Lot A',
+    spotsTaken: 10,
+    totalSpots: 30
+  },
+  {
+    coordinates: [-93.241709, 44.964796],
+    name: 'Lot B',
+    spotsTaken: 8,
+    totalSpots: 20
+  },
+  {
     coordinates: [-93.243197, 44.965065],
     name: 'Lot D',
     spotsTaken: 23,
@@ -20,16 +32,34 @@ const locations = [
 
   },
   {
-    coordinates: [-93.241709, 44.964796],
-    name: 'Unknown Lot',
-    spotsTaken: 8,
+    coordinates: [-93.242081, 44.965318],
+    name: 'Lot E',
+    spotsTaken: 5,
+    totalSpots: 20
+  },
+  {
+    coordinates: [-93.242024, 44.967266],
+    name: 'Lot G',
+    spotsTaken: 18,
+    totalSpots: 30,
+  },
+  {
+    coordinates: [-93.239643, 44.966379],
+    name: 'Lot J',
+    spotsTaken: 20,
+    totalSpots: 30
+  },
+  {
+    coordinates: [-93.238787,44.966341],
+    name: 'Lot K',
+    spotsTaken: 12,
     totalSpots: 20
   },
   {
     coordinates: [-93.237113, 44.965077],
     name: 'Lot L',
     spotsTaken: 15,
-    totalSpots: 30
+    totalSpots: 60
   },
   // ... more locations
 ];
