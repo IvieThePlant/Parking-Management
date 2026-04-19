d3379ab105e4ccf79713ae83b8b2721f2bf9dd9e
Nur Atto <atton@augsburg.edu>
2026-04-19 12:36:27 -0500

    ADD: Implement create_session func to store users session

--- parking/views.py
+++ parking/views.py
@@ -1,6 +1,9 @@
-from django.shortcuts import render
+from django.contrib.auth.models import User
+from django.contrib.sessions.models import Session
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
-from .models import ParkingLot
+from .models import ParkingLot, ParkingSession
+
 
 #Create your views here
 
@@ -8,3 +11,22 @@ from .models import ParkingLot
 def dashboard_view(request):
     lots = ParkingLot.objects.all()
     return render(request, 'parking/dashboard.html', {'lots': lots})
+
+@login_required
+def create_session(request):
+
+    #If API Method call is POST
+    if request.method == 'POST':
+        lot = request.POST.get("lot")
+        license = request.POST.get("license")
+
+        #GET parking lot from ParkingLots table
+        lot = ParkingLot.objects.get(id=lot)
+
+        # Create Session with information from form
+        ParkingSession.objects.create(user=request.user, lot=lot, license=license)
+
+        #Upon Successful insert redirect to mySession page (LANDING PAGE FOR NOW UNTIL MYSESSIONS HTML IS COMPLETE)
+        return redirect('landing')
+
+    return render(request, 'landing.html')
