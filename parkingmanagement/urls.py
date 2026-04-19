26ec1840afe80fd221419e19cdfaebdcfe1b14b0
Nur Atto <atton@augsburg.edu>
2026-04-19 12:36:59 -0500

    ADD: CONFIGURE FUNC PATH

--- parkingmanagement/urls.py
+++ parkingmanagement/urls.py
@@ -18,11 +18,13 @@ from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import TemplateView
 from parking import views as parking_views
-
+from parking.views import create_session
 
 urlpatterns = [
     path('', TemplateView.as_view(template_name='landing.html'), name='landing'),
     path('admin/', admin.site.urls),
     path('accounts/', include('features.accounts.urls')),
     path('dashboard/', parking_views.dashboard_view, name='dashboard'),
+    path("create-session/", create_session, name="create_session"),
+
 ]
