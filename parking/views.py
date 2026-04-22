f27a52ba27bc30a04e94dae496020fcc59f31ed2
Nur Atto <atton@augsburg.edu>
2026-04-22 17:09:43 -0500

    ADD: Add create_session and end_session to store and update session in db

--- parking/views.py
+++ parking/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
-from .models import ParkingLot
+from .models import ParkingLot, ParkingSession
+
 
 #Create your views here
 
@@ -8,3 +10,37 @@ from .models import ParkingLot
 def dashboard_view(request):
     lots = ParkingLot.objects.all()
     return render(request, 'parking/dashboard.html', {'lots': lots})
+
+
+@login_required
+def create_session(request):
+
+    #If API Method call is POST
+    if request.method == 'POST':
+        lot = request.POST.get("lot")
+
+        #GET parking lot from ParkingLots table
+        lot = ParkingLot.objects.get(id=lot)
+
+        #USER ONLY HAS 1 ACTIVE SESSION
+        active_session = ParkingSession.objects.filter(user=request.user, ended_at=None).first()
+        if active_session:
+            messages.error(request, "You already have an active parking session.");
+            return redirect('dashboard')
+
+        # Create Session with information from form
+        ParkingSession.objects.create(user=request.user, lot=lot)
+        messages.success(request, "Parking session created successfully.")
+
+        #Upon Successful insert redirect to mySession page (LANDING PAGE FOR NOW UNTIL MYSESSIONS HTML IS COMPLETE)
+        return redirect('dashboard')
+
+    return render(request, 'dashboard.html')
+
+@login_required
+def end_session(request, session_id):
+    session = ParkingSession.objects.get(ParkingSession, id=session_id, user=request.user)
+
+    session.end_session()
+
+    return redirect('dashboard')
